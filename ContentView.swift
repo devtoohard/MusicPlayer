dc9b42f1ee207e70c47d881f158705f2d81cbe1b
Shawn <devcorehd@gmail.com>
2020-02-01 21:15:11 +0800

    First commit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MuiscPlayer
+//
+//  Created by Shawn on 2/1/20.
+//  Copyright © 2020 Shawn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
